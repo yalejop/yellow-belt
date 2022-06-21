775d462dfbd232dfabc9511f041362fee8a502b8
yalejop <alejo.9211@hotmail.com>
2022-06-21 07:22:08 -0500

    upload project

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var cookie = document.querySelector('#cookie');
+
+function deleteCookie(){
+    cookie.remove()
+}
+
+var image = document.querySelector('#image');
+
+function changeImage(){
+    image.src = 'images/succulents-2.jpg'
+}
+
+function changeImage2(){
+    image.src = 'images/succulents-1.jpg'
+}
